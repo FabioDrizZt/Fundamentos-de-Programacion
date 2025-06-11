da46ce75e79c1a9a459d4ce2e6e15de20ffcd9f1
fabio <fabio@digitalmind.com>
2025-06-10 23:03:01 -0300

    Clase 9

---
+++ Clase 9 - Estructuras Repetitivas/While.js
@@ -0,0 +1,14 @@
+let index = 1
+let suma = 0
+let contador = 0
+
+while (index <= 100) {
+    console.log('iteración n°: ' + index)
+    suma += index // suma = suma + index
+    index += 2   
+    contador++ 
+}
+
+console.log("La suma de los numeros pares del 1 al 100 es: " + suma)
+const promedio = suma / contador
+console.log("El promedio de los numeros pares del 1 al 100 es: " + promedio)
